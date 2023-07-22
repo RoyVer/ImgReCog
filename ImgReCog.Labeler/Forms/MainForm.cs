1c26fd401e7f69a2fc596cba5d14ca0b3f2c5a3f
RoyVer <royverdonk@gmail.com>
2023-07-22 10:49:36 +0200

    Add project files.

---
+++ ImgReCog.Labeler/Forms/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgReCog.Labeler.Forms
+{
+  public class MainForm : Form
+  {
+    private MenuStrip menuStrip;
+    private ToolStripTextBox intervalTextBox;
+    private ToolStripComboBox windowsComboBox;
+    private ToolStripButton viewImagesButton;
+    private PictureBox pictureBox;
+
+    public MainForm()
+    {
+      Text = "Image Labeler";
+      Width = 800;
+      Height = 600;
+
+      menuStrip = new MenuStrip();
+      intervalTextBox = new ToolStripTextBox();
+      windowsComboBox = new ToolStripComboBox();
+      viewImagesButton = new ToolStripButton("View Images");
+      pictureBox = new PictureBox();
+
+      menuStrip.Items.Add(new ToolStripLabel("Interval:"));
+      menuStrip.Items.Add(intervalTextBox);
+      menuStrip.Items.Add(new ToolStripLabel("Window:"));
+      menuStrip.Items.Add(windowsComboBox);
+      menuStrip.Items.Add(viewImagesButton);
+
+      pictureBox.Dock = DockStyle.Fill;
+
+      Controls.Add(menuStrip);
+      Controls.Add(pictureBox);
+
+      viewImagesButton.Click += ViewImagesButton_Click;
+    }
+
+    protected override void OnShown(EventArgs e)
+    {
+      base.OnShown(e);
+
+      // Populate the dropdown with the currently open windows
+      foreach (Process process in Process.GetProcesses())
+      {
+        if (!string.IsNullOrEmpty(process.MainWindowTitle))
+        {
+          windowsComboBox.Items.Add(process.MainWindowTitle);
+        }
+      }
+    }
+
+    private void ViewImagesButton_Click(object sender, EventArgs e)
+    {
+      ImagesForm imagesForm = new ImagesForm();
+      imagesForm.Show();
+    }
+  }
+}
---
+++ ImgReCog.Labeler/Screen/Capture.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ImgReCog.Labeler.Screen
+{
+  public static class Capture
+  {
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    public static extern bool GetWindowRect(IntPtr hWnd, out RECT rect);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+      public int Left;
+      public int Top;
+      public int Right;
+      public int Bottom;
+    }
+
+    public static Bitmap CaptureScreenshot(IntPtr windowHandle)
+    {
+      if (GetWindowRect(windowHandle, out RECT rect))
+      {
+        int width = rect.Right - rect.Left;
+        int height = rect.Bottom - rect.Top;
+
+        Bitmap bitmap = new Bitmap(width, height);
+        using (Graphics graphics = Graphics.FromImage(bitmap))
+        {
+          graphics.CopyFromScreen(rect.Left, rect.Top, 0, 0, new Size(width, height));
+        }
+
+        return bitmap;
+      }
+
+      throw new Exception("Unable to capture screenshot.");
+    }
+
+    public static void SaveScreenshot(Bitmap bitmap, string windowName)
+    {
+      string date = DateTime.Now.ToString("yyyyMMdd");
+      string time = DateTime.Now.ToString("HHmmss");
+
+      string directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "unprocessed", windowName, date);
+      Directory.CreateDirectory(directory);
+
+      string filename = Path.Combine(directory, $"{windowName}_{time}.jpg");
+
+      try
+      {
+        bitmap.Save(filename, ImageFormat.Jpeg);
+      }
+      catch (Exception ex)
+      {
+        throw new Exception("Unable to save screenshot.", ex);
+      }
+    }
+  }
+}
